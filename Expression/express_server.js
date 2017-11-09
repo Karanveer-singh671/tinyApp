ed681d965c596bb40ad4f7cc8018e91a990e3be7
Karanveer-singh671 <singhkaranveer671@gmail.com>
2017-11-09 04:13:20 +0000

    tiny app log

--- Expression/express_server.js
+++ Expression/express_server.js
@@ -1,89 +1,98 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
-app.set("view engine", "ejs")
-
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require("cookie-parser");
 
-// make delete dynamic
- function generateRandomString() {
- return Math.random().toString(36).substr(2, 6)
-
+function generateRandomString() {
+  return Math.random().toString(36).substr(2,6);
 }
+
+app.set("view engine", "ejs")
+app.use(cookieParser());
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-
+  "9sm5xK": "http://www.google.com"
 };
-// defining (registering) a HTTP GET request (since server doesn't request) on /
-// Along with a callback func that will handle the request
+
+// urlDatabase[id] = stuff here;
+
+// middleware
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// specific routes
 app.get("/", (req, res) => {
-  res.end("Hello!");
-});
-
-
-
-app.post("/urls/:id/delete", (req,res) => {
-let id = req.params.id;
-delete urlDatabase[id];  // need to access w/ in object or else won't work!
-res.redirect("/urls");
-});
-
-app.post("/urls/:id", (req,res) => {
-  let shortURL = req.params.id;
-  let longURL = req.body["longURL"]
-  urlDatabase[shortURL] = longURL;
-   // don't modify from inside object can change from outside
- // console.log(urlDatabase);
-  res.redirect("/urls" );
-})
-
-app.post("/urls", (req, res) => {
-  console.log(generateRandomString(), req.body.longURL);  // debug statement to see POST parameters
-  let shortURL = generateRandomString();
-  let longURL = req.body["longURL"]
-  urlDatabase[shortURL] = longURL;
-   // don't modify from inside object can change from outside
- // console.log(urlDatabase);
-  res.redirect("/urls" );         // Respond with 'Ok' (we will replace this)
+  res.send("Hello!");
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase,
+                      username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => { // if you put new below id it thinks new is the id
-  let id = req.params.id;
-  res.render("urls_new");
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let shortURL = generateRandomString();
+  let longURL = req.body["longURL"];
+  urlDatabase[shortURL] = longURL;
+  console.log(shortURL);
+  res.redirect("/urls/");
 });
 
-app.get("/urls/:id", (req, res) => {  // not a response it's used to get the response (tool to make)
+app.get("/urls/new", (req, res) => {
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
-                        urlDatabase: urlDatabase };
-       let id = req.params.id;  // accesses short url
-       //res.redirect(urlDatabase[id]); // responds with a redirection with the value associated with id which is URL
+                       urlDatabase: urlDatabase,
+                       username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-   let longURL = urlDatabase[req.params.shortURL]; // req.params.shortURL but access form w/ in object so urlDatabase[req.params.shortURL]
-   res.redirect(longURL);
+app.post("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  let longURL = req.body["longURL"];
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  console.log(urlDatabase[req.params.shortURL]);
+  res.redirect(longURL);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  let id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls");
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username",req.body.username)
+  console.log(req.body)
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username", req.body.username)
+  console.log(req.body.username)
+  res.redirect("/urls")
+})
+
+
+// app.post("/urls/:id", (req, res) => {
+//   let id = req.params.id
+//   res.redirect(`/urls/{$id}`)
+// }
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
